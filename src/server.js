418f5ab2178b0bc27d3c2d42409488cde7973928
Lukianov Illia <Lukjanovilly@gmail.com>
2025-08-28 09:48:46 +0300

    5

--- src/server.js
+++ src/server.js
@@ -12,32 +12,14 @@ import { UPLOAD_DIR } from './constants/index.js';
 import path from 'node:path';
 
 const PORT = getEnvVariables("PORT") ?? "3000";
-const allowedOrigins = [
-'http://localhost:5173',
-'https://united-team-finally-project-front-e.vercel.app',
-];
 
 export default function setupServer() {
   const app = express();
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true,
-// }));
   app.use(
-    cors({
-      origin: (origin, callback) => {
-        callback(null, origin || '*'); // дозволяє будь-який домен
-      },
-      credentials: true,
-    }),
-  );
+  cors({
+    origin: "https://test-tau-ochre-15.vercel.app",
+    credentials: true,
+  }));
   app.use(express.json());
   app.use(
     pino({
